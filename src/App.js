eec9fd907e2ae86e3a737708b9675e55adb791ae
takumiiimo <assistant2370@gmail.com>
2021-06-04 17:53:34 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 // 関数コンポーネント
 const App = () => {
   const profiles = [
-    { name: "Takumi", age: "27" },
-    { name: "Minako", age: "37" },
-    { name: "Noname" }
+    { name: "Takumi", age: 27 },
+    { name: "Minako", age: 37 },
+    { name: "NoName", age: 99 }
   ]
   return (
 
@@ -22,11 +23,11 @@ const App = () => {
 }
 
 const User = (props) => {
-  return <div>Hi, I'm {props.name}, and {props.age}years old. </div>
+  return <div>Hi, I'm {props.name}, and {props.age} years old. </div>
 }
-
-User.defaultProps = {
-  age: 27
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
