060cd190a4b4a395dc7ea0e1b445a22eab9127c8
takumiiimo <assistant2370@gmail.com>
2021-06-04 17:26:08 +0900

    create class component and function component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from "react";
-// const greeting = "Hi";
-// const dom = <h1 className="foo">{greeting} minako</h1>;
 
-function App() {
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">
+//         bar
+//       </label>
+//       < input type="text" onChange={() => { console.log("takumiiimo") }} />
+//     </React.Fragment>
+//   )
+
+// }
+
+// 関数コンポーネント
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">
-        bar
-      </label>
-      < input type="text" onChange={() => { console.log("takumiiimo") }} />
-    </React.Fragment>
-  )
 
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow</div>
 }
 
 export default App;
