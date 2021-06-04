58fdd4dbf37e217487439d106d593e3c1e2bc230
takumiiimo <assistant2370@gmail.com>
2021-06-04 16:38:34 +0900

    learn more-jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// const greeting = "Hi";
+// const dom = <h1 className="foo">{greeting} minako</h1>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <label htmlFor="bar">
+        bar
+      </label>
+      < input type="text" onChange={() => { console.log("takumiiimo") }} />
+    </React.Fragment>
+  )
+
 }
 
 export default App;
