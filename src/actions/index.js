271dd4910776ef4df6cefce4cd8d4597d5ff1267
takumiiimo <assistant2370@gmail.com>
2021-06-05 15:54:33 +0900

    fix typo for actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,6 @@ export const DECREMENT = "DECREMENT";
 export const increment = () => ({
     type: INCREMENT
 })
-export const increment = () => ({
+export const decrement = () => ({
     type: DECREMENT
 })
